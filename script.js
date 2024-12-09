7b1fbee0f6a526bcb11f05aa248c297bea58c398
Daniel Lichotti <danielclichotti@gmail.com>
2024-12-08 21:52:00 -0300

    Enviando scripts

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
 
+//captura o evento de input para formatar o valor
 amount.oninput = () => {
-  alert('foi')
+  //obtem o valor atual do input e remove os caracteres nao numericos
+  let value = amount.value.replace(/\D/g, '')
+
+  //transformar o valor em centavos
+  value = Number(value)/100
+
+  //atualiza o valor do input
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+  //formata o valor padrao brl que é brasileiro
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  return value
+}
+
+//captura o evento de submit do formulario para obter os valores
+form.onsubmit = (event) => {
+  //previne o comportamento padrao de recarregar a pagina
+  event.preventDefault()
+
+  //cria um objeto com os detalhes na nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    create_at: new Date(),
+  }
 }
